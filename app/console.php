da50a25dafac2043a00838eedab4b8f35e0aa742
Petr Bechyně <mail@petrbechyne.com>
2017-03-15 19:44:34 +0100

    Symfony wiring introduced: DI Container + Yaml service configuration

--- app/console.php
+++ app/console.php
@@ -3,38 +3,19 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-// @todo: wiring should be done better :)
-$randomOrganismTypeGetter = new \LivingWorld\Generate\XmlFile\Structure\Organism\RandomOrganismTypeGetter();
-$worldStructureGenerator = new \LivingWorld\Generate\XmlFile\WorldStructureGenerator($randomOrganismTypeGetter);
-$xmlFileGetter = new \LivingWorld\Generate\XmlFile\XmlFileGetter();
-$xmlFileSaver = new \LivingWorld\Generate\XmlFile\XmlFileSaver(__DIR__.'/../tmp');
-$xmlFileParser = new \LivingWorld\Import\XmlFile\XmlFileParser(__DIR__.'/../tmp');
-$organismTypeFormatter = new \LivingWorld\Graphics\OrganismTypeFormatter();
-$organismOutputFormatter = new \LivingWorld\Graphics\OrganismOutputFormatter();
-$screenWriter = new \LivingWorld\Graphics\Screen\ScreenWriter($organismTypeFormatter);
-$importFileProcessor = new \LivingWorld\Import\Processor\ImportFileProcessor();
-$worldStructureGetter = new \LivingWorld\Import\XmlFile\Output\WorldStructureGetter();
+$container = new ContainerBuilder();
+$container->setParameter('tmp.dir', __DIR__.'/../tmp');
+
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
+$loader->load('services.yml');
 
 $application = new Application();
-$application->add(
-    new \LivingWorld\Import\ImportCommand(
-        $organismOutputFormatter,
-        $xmlFileParser,
-        $importFileProcessor,
-        $worldStructureGetter,
-        $xmlFileGetter,
-        $xmlFileSaver,
-        $screenWriter
-    )
-);
-$application->add(
-    new \LivingWorld\Generate\GenerateWorldCommand(
-        $worldStructureGenerator,
-        $xmlFileGetter,
-        $xmlFileSaver
-    )
-);
+$application->add($container->get('import.command'));
+$application->add($container->get('generate.world.command'));
 
 $application->run();
